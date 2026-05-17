f68f34e52e217ca84a07768a5c0f07a1871d5fed
djalil-jijo <42841932+djalil-jijo@users.noreply.github.com>
2026-05-17 05:21:35 +0100

    build: update next.config.ts configuration settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // 👈 أضف هذا السطر هنا
+  images: {
+    unoptimized: true, // 👈 يفضل إضافة هذا السطر أيضاً لمنع أخطاء الصور الثابتة في الـ export
+  },
 };
 
 export default nextConfig;
